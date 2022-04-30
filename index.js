94ce0eb6f0d51d49949363847b8894b93b67353c
Daniel Liu <danielliu524@ucla.edu>
2022-04-30 12:20:45 -0700

    Initialize repo

---
+++ index.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+require("dotenv").config()
+
+const client = new Discord.Client({
+    intents: [
+        "GUILDS",
+        "GUILD_MESSAGES"
+    ]
+})
+
+client.on("ready", () => {
+    console.log("Logged in")
+})
+
+client.login(process.env.TOKEN);
